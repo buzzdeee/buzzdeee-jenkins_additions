613b6d7d7edcb871b44357b9cb4f31afa7966edf
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2016-11-04 14:30:40 +0100

    Allow configuration of the jabber plugin

--- manifests/init.pp
+++ manifests/init.pp
@@ -39,9 +39,25 @@ class jenkins_additions (
   $jenkins_url,
   $jenkins_path = undef,
   $admin_address,
+  $jabber_enabled = 'false',
+  $jabber_port = '5222',
+  $jabber_jid = 'jenkins@example.com',
+  $jabber_password = 'password',
+  $jabber_nickname = 'jenkins',
+  $jabber_expose_presence = 'true',
+  $jabber_accept_all_certs = 'false',
+  $jabber_scramble_passwords = 'true',
+  $jabber_command_prefix = '!',
+  $jabber_default_suffix = '',
+  $jabber_subscription_mode = 'accept_all',
+  $jabber_email_address_jabber_id = 'false',
+  $jabber_proxy_type = 'NONE',
+  $jabber_proxy_port = '3128',
 ) {
 
   include jenkins
+
+  $jabber_encoded_password = base64('encode', $jabber_password)
 
   file { "${::jenkins::localstatedir}/jenkins.model.JenkinsLocationConfiguration.xml":
     owner   => "${::jenkins::user}",
@@ -49,9 +65,16 @@ class jenkins_additions (
     mode    => '0644',
     content => template('jenkins_additions/jenkins.model.JenkinsLocationConfiguration.xml.erb'),
   }
+  file { "${::jenkins::localstatedir}/hudson.plugins.jabber.im.transport.JabberPublisher.xml":
+    owner   => "${::jenkins::user}",
+    group   => "${::jenkins::group}",
+    mode    => '0644',
+    content => template('jenkins_additions/hudson.plugins.jabber.im.transport.JabberPublisher.xml.erb'),
+  }
 
   Package[$::jenkins::package_name] ->
   File["${::jenkins::localstatedir}/jenkins.model.JenkinsLocationConfiguration.xml"] ~>
+  File["${::jenkins::localstatedir}/hudson.plugins.jabber.im.transport.JabberPublisher.xml"] ~>
   Service['jenkins']
 
 }
